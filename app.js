ac504bd734dcb1199fb28be15365064d68d6553e
William Forero <williamforerob@gmail.com>
2023-09-05 12:06:29 -0500

    first version

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3030; // Puedes cambiar el puerto según tus necesidades
+
+// Configura Express para servir archivos estáticos desde un directorio llamado "public"
+app.use(express.static('public'));
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+  });
+  
+  // Inicia el servidor en el puerto especificado
+  app.listen(PORT,"0.0.0.0", () => {
+    console.log(`El servidor está escuchando en http://localhost:${PORT}`);
+  });
