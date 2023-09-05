f4367d5edfd04a2e937222ccaf80efc11846d0d2
William Forero <williamforerob@gmail.com>
2023-09-05 14:38:12 -0500

    fix routing

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ app.use(express.static('public'));
 
 // Ruta de ejemplo
 app.get('/', (req, res) => {
-    res.send('¡Hola, mundo!');
-  });
-  
-  // Inicia el servidor en el puerto especificado
-  app.listen(PORT,"0.0.0.0", () => {
-    console.log(`El servidor está escuchando en http://localhost:${PORT}`);
-  });
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Ruta que maneja todas las demás rutas de Angular
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Inicia el servidor en el puerto especificado
+app.listen(PORT,"0.0.0.0", () => {
+  console.log(`El servidor está escuchando en http://localhost:${PORT}`);
+});
